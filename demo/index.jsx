67ffa05e38ce304e415dcf26d86717fa31ceeeba
recover758126 <761647716@qq.com>
2019-07-29 09:40:33 +0800

    refactor: update demo

--- demo/index.jsx
+++ demo/index.jsx
@@ -1,23 +1,22 @@
-import {createElement, render,Component } from 'rax';
-import DU from "driver-universal"
+/* eslint-disable import/no-extraneous-dependencies */
+import { createElement, render } from 'rax';
+import DU from 'driver-universal';
 import View from 'rax-view';
 import QRCode from '../src/index';
 
-class App extends Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    return (
-      <View style={{flex: 1}}>
-	    <QRCode 
-           data={'http://market.m.taobao.com/apps/market/m-vip/88-festival.html?wh_weex=true&wx_navbar_transparent=true'}
-          style={{width: 400, height: 400}}
-        />
-      </View>
-    );
-  }
+function App() {
+  return (
+    <View style={{ flex: 1 }}>
+      <QRCode
+        data="http://market.m.taobao.com/apps/market/m-vip/88-festival.html?wh_weex=true&wx_navbar_transparent=true"
+        style={{ width: 400, height: 400 }}
+      />
+      <QRCode
+        data="http://market.m.taobao.com/apps/market/m-vip/88-festival.html?wh_weex=true&wx_navbar_transparent=true"
+        options={{ fillColor: 'red', blankColor: '#000' }}
+      />
+    </View>
+  );
 }
 
 render(<App />, document.body, { driver: DU });
